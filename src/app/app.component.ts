b97e72da9b06d88daa48f3bbf99fcbb038e86f07
Tayllor Lloyd <tayllorlloyd@Tayllors-MacBook.local>
2020-04-17 02:16:26 -0500

    step 8 commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Satellite } from './satellite';
+import { SelectorMatcher } from '@angular/compiler';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +10,7 @@ import { Satellite } from './satellite';
 export class AppComponent {
   title = 'orbit-report';
   sourceList: Satellite[];
-
+  displayList: Satellite[];
   constructor() {
     this.sourceList = [];
     let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
@@ -22,8 +23,22 @@ export class AppComponent {
            let satellite = new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
            this.sourceList.push(satellite);
           }
+          this.displayList = this.sourceList.slice(0);
        }.bind(this));
     }.bind(this));
  
  }
+ search(searchTerm: string): void {
+  let matchingSatellites: Satellite[] = [];
+  searchTerm = searchTerm.toLowerCase();
+  for(let i=0; i < this.sourceList.length; i++) {
+     let name = this.sourceList[i].name.toLowerCase();
+     if (name.indexOf(searchTerm) >= 0) {
+        matchingSatellites.push(this.sourceList[i]);
+     } 
+  }
+
+  this.displayList = matchingSatellites;
+
+}
 }
